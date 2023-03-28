e177dfce68999d886e55e4884ae6a4e246d11610
Alexandre Gomez <asgomez@msn.com>
2023-03-28 09:24:34 -0300

    Implementando validação de classes

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,10 +1,11 @@
 ﻿using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido : Entidade
     {
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
@@ -23,5 +24,16 @@ namespace QuickBuy.Dominio.Entidades
         /// ou muitos itens de pedidos
         /// </summary>
         public ICollection<ItemPedido> ItensPedido { get; set; }
+
+        public override void Validate()
+        {
+            LimparMensagensValidcao();
+
+            if (!ItensPedido.Any())
+                AdicionarCritica("Critica - Pedido não pode ficar sem item de pedido");
+            
+            if(string.IsNullOrEmpty(CEP))
+                AdicionarCritica("Critica - CEP deve estar preenchido");
+        }
     }
 }
